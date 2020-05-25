ad78c8bb9d48b635ab57ae8677a1cf1589387501
Ding <ding533@hotmail.com>
2020-05-26 00:38:47 +0700

    update BeagleDog

--- src/decorator/BeagleDog.java
+++ src/decorator/BeagleDog.java
@@ -1,50 +1,23 @@
-//public class BeagleDog implements Dog {
-//    private String name;
-//    private String breed;
-//    private int age;
-//
-//    public BeagleDog (String name, int age) {
-//        this.name = name;
-//        this.breed = "Beagle";
-//        this.age = age;
-//    }
-//
-//    @Override
-//    public void bark() {
-//        System.out.println(breed + " Bark");
-//    }
-//
-//    @Override
-//    public void walk() {
-//        System.out.println(breed + " Walk");
-//    }
-//
-//    @Override
-//    public void run() {
-//        System.out.println(breed + " Run");
-//    }
-//
-//    @Override
-//    public void jump() {
-//        System.out.println(breed + " Jump");
-//    }
-//
-//    @Override
-//    public void stand() {
-//        System.out.println(breed + " Stand");
-//    }
-//
-//    @Override
-//    public void printBehaviours() {
-//        bark();
-//        walk();
-//        run();
-//        jump();
-//        stand();
-//    }
-//
-//    @Override
-//    public String getDescription() {
-//        return "I am " + age + "years old " + breed + " dog named " + name + ".";
-//    }
-//}
+package decorator;
+
+public class BeagleDog extends Dog {
+    private String name;
+    private int age;
+    private String breed;
+    private String description;
+
+    public BeagleDog  (String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.breed = "Beagle";
+        description = "I am " + age + " years old " + breed + " dog named " + name + ".";
+    }
+
+    @Override
+    public void activeAllBehaviours() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
