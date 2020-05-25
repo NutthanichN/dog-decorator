7bfc675c11a620f29ef55e85b90f43917b98af09
Tetach Rattanavikran <47658385+theethaj@users.noreply.github.com>
2020-05-26 00:30:47 +0700

    improve AlaskanDog.java and BullDog.java

---
+++ src/BulldogDog.java
@@ -0,0 +1,23 @@
+package decorator;
+
+public class BulldogDog extends Dog {
+    private String name;
+    private int age;
+    private String breed;
+    private String description;
+
+    public BulldogDog (String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.breed = "Bulldog";
+        description = "I am " + age + " years old " + breed + " dog named " + name + ".";
+    }
+
+    @Override
+    public void activeAllBehaviours() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
