293a26f5925d3b0a2681b0804bc419e54ab9d980
Tetach Rattanavikran <47658385+theethaj@users.noreply.github.com>
2020-05-26 14:11:41 +0700

    add AlaskanDog.java and BulldogDog.java

--- src/decorator/BulldogDog.java
+++ src/decorator/BulldogDog.java
@@ -1,23 +1,13 @@
 package decorator;
 
 public class BulldogDog extends Dog {
-    private String name;
-    private int age;
-    private String breed;
-    private String description;
-
     public BulldogDog (String name, int age) {
-        this.name = name;
-        this.age = age;
+        super(name, age);
         this.breed = "Bulldogs";
-        description = "I am " + age + " years old " + breed + " dog named " + name + ".";
     }
 
     @Override
-    public void activeAllBehaviours() {
-    }
-
     public String getDescription() {
-        return description;
+        return "I am " + age + " years old " + breed + " dog named " + name + ".";
     }
 }
