1e45467bb6a7f4ec533e2c5bbdd4e5fcd59add03
Nutthanich Narphromar <monaprom134@gmail.com>
2020-05-26 23:10:57 +0700

    Update Main)

--- src/decorator/Main.java
+++ src/decorator/Main.java
@@ -3,11 +3,15 @@ package decorator;
 public class Main {
     public static void main(String[] args) {
         Dog c = new ChihuahuaDog("Dukdik", 10);
-//        Dog be = new BeagleDog("JackJack", 7);
+        Dog be = new BeagleDog("JackJack", 7);
+//        Dog bu = new BulldogDog("Marry", 4);
 //        Dog s = new SamoyedDog("Daniel", 3);
+//        Dog a = new AlaskanDog("Bob", 1);
 
         System.out.println(c.getDescription());
         c.walk();
+        c.stand();
+        System.out.println("---------------------------");
 
         c = new ChicStandingDog(c);
         c = new HappyWalkingDog(c);
@@ -15,9 +19,12 @@ public class Main {
         c.walk();
         c.stand();
 
-
-//        c = new ExcitingRunningDog(c);
-//        System.out.println(c.getDescription());
+        System.out.println(be.getDescription());
+        be.run();
+        System.out.println("---------------------------");
+        be = new FastRunningDog(be);
+        System.out.println(be.getDescription());
+        be.run();
 
     }
 }
