dbaf5b7f677c5e5cbf12a1e2f704d71b41b34b3c
Ding <ding533@hotmail.com>
2020-05-26 13:00:07 +0700

    Add beagle and samoyed

--- src/decorator/Main.java
+++ src/decorator/Main.java
@@ -29,5 +29,61 @@ public class Main {
         c = new StandingDog(c);
         System.out.println(c.getDescription());
         c.activeAllBehaviours();
+
+        Dog be = new BeagleDog("JackJack", 7);
+        System.out.println(be.getDescription());
+        be.activeAllBehaviours();
+
+        System.out.println();
+
+        be = new DancingDog(be);
+        System.out.println(be.getDescription());
+        be.activeAllBehaviours();
+
+        System.out.println();
+
+        be = new RunningDog(be);
+        System.out.println(be.getDescription());
+        be.activeAllBehaviours();
+
+        System.out.println();
+
+        be = new PlayingDeadDog(be);
+        System.out.println(be.getDescription());
+        be.activeAllBehaviours();
+
+        System.out.println();
+
+        be = new StandingDog(be);
+        System.out.println(be.getDescription());
+        be.activeAllBehaviours();
+
+        Dog s = new SamoyedDog("Daniel", 3);
+        System.out.println(s.getDescription());
+        s.activeAllBehaviours();
+
+        System.out.println();
+
+        s = new DancingDog(s);
+        System.out.println(s.getDescription());
+        s.activeAllBehaviours();
+
+        System.out.println();
+
+        s = new RunningDog(s);
+        System.out.println(s.getDescription());
+        s.activeAllBehaviours();
+
+        System.out.println();
+
+        s = new PlayingDeadDog(s);
+        System.out.println(s.getDescription());
+        s.activeAllBehaviours();
+
+        System.out.println();
+
+        s = new StandingDog(s);
+        System.out.println(s.getDescription());
+        s.activeAllBehaviours();
     }
 }
