a553efee04ecbac6411939369d4a076cae2a419e
Nutthanich Narphromar <monaprom134@gmail.com>
2020-05-26 14:04:34 +0700

    Implement new Dog

--- src/decorator/ChihuahuaDog.java
+++ src/decorator/ChihuahuaDog.java
@@ -1,23 +1,13 @@
 package decorator;
 
 public class ChihuahuaDog extends Dog {
-    private String name;
-    private int age;
-    private String breed;
-    private String description;
-
     public ChihuahuaDog (String name, int age) {
-        this.name = name;
-        this.age = age;
+        super(name, age);
         this.breed = "Chihuahua";
-        description = "I am " + age + " years old " + breed + " dog named " + name + ".";
     }
 
     @Override
-    public void activeAllBehaviours() {
-    }
-
     public String getDescription() {
-        return description;
+        return "I am " + age + " years old " + breed + " dog named " + name + ".";
     }
 }
--- src/decorator/Dog.java
+++ src/decorator/Dog.java
@@ -1,11 +1,37 @@
 package decorator;
 
 public abstract class Dog {
-    private String description = "Unknown";
+    protected String name;
+    protected int age;
+    protected String breed;
 
-    public String getDescription() {
-        return description;
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public abstract String getDescription();
+
+    public void bark() {
+        System.out.println("Barking");
     }
 
-    public abstract void activeAllBehaviours();
+    public void walk() {
+        System.out.println("Walking");
+    }
+
+    public void run() {
+        System.out.println("Running");
+    }
+
+    public void stand() {
+        System.out.println("Standing");
+    }
+
+    public void sit() {
+        System.out.println("Sitting");
+    }
+
+    public void sleep() {
+        System.out.println("Sleeping");
+    }
 }
