8a3f282db08b36467b7025782b2f0b9cfb92afb9
Ding <ding533@hotmail.com>
2020-05-26 14:07:32 +0700

    update beagle

--- src/decorator/BeagleDog.java
+++ src/decorator/BeagleDog.java
@@ -1,23 +1,14 @@
 package decorator;
 
 public class BeagleDog extends Dog {
-    private String name;
-    private int age;
-    private String breed;
-    private String description;
-
-    public BeagleDog  (String name, int age) {
-        this.name = name;
-        this.age = age;
+    public BeagleDog (String name, int age) {
+        super(name, age);
         this.breed = "Beagle";
-        description = "I am " + age + " years old " + breed + " dog named " + name + ".";
     }
 
     @Override
-    public void activeAllBehaviours() {
-    }
-
     public String getDescription() {
-        return description;
+        return "I am " + age + " years old " + breed + " dog named " + name + ".";
     }
 }
+
