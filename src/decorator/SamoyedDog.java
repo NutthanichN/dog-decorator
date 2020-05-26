835caddea466c1f80fd64403a0b86bcb19e9467e
Ding <ding533@hotmail.com>
2020-05-26 14:08:03 +0700

    update samoyed

--- src/decorator/SamoyedDog.java
+++ src/decorator/SamoyedDog.java
@@ -1,23 +1,15 @@
 package decorator;
 
 public class SamoyedDog extends Dog {
-    private String name;
-    private int age;
-    private String breed;
-    private String description;
-
     public SamoyedDog (String name, int age) {
-        this.name = name;
-        this.age = age;
+        super(name, age);
         this.breed = "Samoyed";
-        description = "I am " + age + " years old " + breed + " dog named " + name + ".";
     }
 
     @Override
-    public void activeAllBehaviours() {
-    }
-
     public String getDescription() {
-        return description;
+        return "I am " + age + " years old " + breed + " dog named " + name + ".";
     }
 }
+
+
