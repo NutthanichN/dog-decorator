02ddf92229ffd4d4c42473a53c37026166fcd2b6
Nutthanich Narphromar <monaprom134@gmail.com>
2020-05-26 15:03:06 +0700

    Add overwrite methods to ChicStandingDog

--- src/decorator/ChicStandingDog.java
+++ src/decorator/ChicStandingDog.java
@@ -12,6 +12,17 @@ public class ChicStandingDog extends DogDecorator {
     }
 
     @Override
+    public void bark() { decoratedDog.bark(); }
+    @Override
+    public void walk() { decoratedDog.walk(); }
+    @Override
+    public void run() { decoratedDog.run(); }
+    @Override
+    public void sit() { decoratedDog.sit(); }
+    @Override
+    public void sleep() { decoratedDog.sleep(); }
+
+    @Override
     public String getDescription() {
         return decoratedDog.getDescription() + " Chicly stand";
     }
